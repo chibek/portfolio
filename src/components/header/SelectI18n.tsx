883236d20825d287fb1c034520d1f99f2987d05c
Sergio Beleña <sergio.belena@wunderdog.fi>
2024-11-20 12:25:43 +0000

    Change styles

--- src/components/header/SelectI18n.tsx
+++ src/components/header/SelectI18n.tsx
@@ -1,5 +1,6 @@
 import { Languages } from "lucide-preact";
 import { useState } from "preact/hooks";
+import { cn } from "../../lib/utils";
 
 export const SelectI18N = () => {
   const languages = [
@@ -11,19 +12,22 @@ export const SelectI18N = () => {
 
   const toggleDropdown = () => setIsOpen(!isOpen);
   return (
-    <div className="relative inline-block text-left">
+    <div className="relative">
       <button
-        class="p-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+        className={cn("flex items-bottom rounded-lg p-2 ring-primary", {
+          "ring-1": isOpen,
+        })}
         onClick={toggleDropdown}
       >
-        <Languages className="size-4" />
+        <Languages className="size-5" />
       </button>
 
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2  rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className="origin-top-right absolute right-0 mt-2 overflow-hidden rounded-md shadow-lg border">
           {languages.map((language) => (
             <a
-              className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100"
+              onClick={toggleDropdown}
+              className="block px-4 py-2 text-sm hover:bg-secondary"
               href={language.href}
             >
               {language.label}
